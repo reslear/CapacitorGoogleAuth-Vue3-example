ed24be5b1b6094b4f4cf660fbd0a1d92c33857ca
reslear <reslear@gmail.com>
2021-08-22 12:44:44 +0300

    feat: init ionic

---
+++ src/views/Login.vue
@@ -0,0 +1,49 @@
+<template>
+  <ion-page>
+    <ion-content :fullscreen="true" class="content">
+      <div class="center">
+        <section>
+          <ion-button expand="block">
+            <img class="google" :src="require('@/assets/google.svg')" />
+            <span>Login with Google</span>
+          </ion-button>
+        </section>
+      </div>
+    </ion-content>
+  </ion-page>
+</template>
+
+<script lang="ts">
+import { IonPage, IonContent, IonButton } from '@ionic/vue'
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  components: {
+    IonContent,
+    IonPage,
+    IonButton,
+  },
+})
+</script>
+
+<style scoped>
+.google {
+  background: #fff;
+  padding: 4px;
+  border-radius: 4px;
+  margin-right: 13px;
+}
+
+.center {
+  height: 100%;
+  width: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  gap: 1ch;
+}
+section {
+  width: 100%;
+  padding: 0 10px;
+}
+</style>
---
+++ src/views/Main.vue
@@ -0,0 +1,33 @@
+<template>
+  <ion-page>
+    <ion-header>
+      <ion-toolbar>
+        <ion-title>Tab 2</ion-title>
+      </ion-toolbar>
+    </ion-header>
+    <ion-content :fullscreen="true">
+      <ion-header collapse="condense">
+        <ion-toolbar>
+          <ion-title size="large">Tab 2</ion-title>
+        </ion-toolbar>
+      </ion-header>
+
+      profile
+    </ion-content>
+  </ion-page>
+</template>
+
+<script lang="ts">
+import {
+  IonPage,
+  IonHeader,
+  IonToolbar,
+  IonTitle,
+  IonContent,
+} from '@ionic/vue'
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  components: { IonHeader, IonToolbar, IonTitle, IonContent, IonPage },
+})
+</script>
